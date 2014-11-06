5258f480804022f20f562d0f86b3547c94774609
Leo <leo90ae@gmail.com>
2014-11-06 01:12:45 -0300

    Added show current locations, now you can add a marker with a longclick on map and starting with database integration (not working)

--- app/src/main/java/com/example/android/markers/MapsActivity.java
+++ app/src/main/java/com/example/android/markers/MapsActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -14,7 +15,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity {
@@ -27,6 +40,8 @@ public class MapsActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
         setUpMapIfNeeded();
     }
 
@@ -101,7 +116,7 @@ public class MapsActivity extends FragmentActivity {
         // Show the current location in Google Map
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mLatLng));
         // Zoom in the Google Map
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
 
 
         // On map long click
@@ -109,13 +124,131 @@ public class MapsActivity extends FragmentActivity {
             @Override
             public void onMapLongClick(LatLng latLng) {
                 addMarker(latLng);
+                sendToServer(latLng);
             }
         });
+
+        // Starting locations retrieve task
+        new RetrieveTask().execute();
     }
 
     // Adding marker on the GoogleMaps
     private void addMarker(LatLng latlng) {
         Marker marker = mMap.addMarker(new MarkerOptions().position(latlng));
         markers.add(marker);
+    }
+
+    // Invoking background thread to store the touched location in Remove MySQL server
+    private void sendToServer(LatLng latlng) {
+        new SaveTask().execute(latlng);
+    }
+
+    // Background thread to save the location in remove MySQL server
+    private class SaveTask extends AsyncTask<LatLng, Void, Void> {
+        @Override
+        protected Void doInBackground(LatLng... params) {
+            String lat = Double.toString(params[0].latitude);
+            String lng = Double.toString(params[0].longitude);
+            String strUrl = "http://192.168.1.140/marker/save.php";
+            URL url = null;
+            try {
+                url = new URL(strUrl);
+
+                HttpURLConnection connection = (HttpURLConnection) url
+                        .openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+                        connection.getOutputStream());
+
+                outputStreamWriter.write("lat=" + lat + "&lng=" + lng);
+                outputStreamWriter.flush();
+                outputStreamWriter.close();
+
+                InputStream iStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new
+                        InputStreamReader(iStream));
+
+                StringBuffer sb = new StringBuffer();
+
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                reader.close();
+                iStream.close();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    // Background task to retrieve locations from remote mysql server
+    private class RetrieveTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String strUrl = "http://192.168.1.140/marker/retrieve.php";
+            URL url = null;
+            StringBuffer sb = new StringBuffer();
+            try {
+                url = new URL(strUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream iStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(iStream));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                reader.close();
+                iStream.close();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            new ParserTask().execute(result);
+        }
+    }
+
+    // Background thread to parse the JSON data retrieved from MySQL server
+    private class ParserTask extends AsyncTask<String, Void, List<HashMap<String, String>>> {
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... params) {
+            MarkerJSONParser markerParser = new MarkerJSONParser();
+            JSONObject json = null;
+            try {
+                json = new JSONObject(params[0]);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            List<HashMap<String, String>> markersList = markerParser.parse(json);
+            return markersList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> result) {
+            for (int i = 0; i < result.size(); i++) {
+                HashMap<String, String> marker = result.get(i);
+                LatLng latlng = new LatLng(Double.parseDouble(marker.get("lat")), Double.parseDouble(marker.get("lng")));
+                addMarker(latlng);
+            }
+        }
     }
 }
